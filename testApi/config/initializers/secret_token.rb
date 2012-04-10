1a50c273c21b4e720d4e61c5c187abdc5043a016
Harold Rangel Charris <harold.rangel@koombea.com>
2012-04-10 12:17:49 -0500

    [feature] creating new user

---
+++ testApi/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestApi::Application.config.secret_token = '66174f77b169bb7fa5a321b40b577406535b4a087dfb10a428e9b21254fd2486a6ef72d3e5875519eeaa0b26fce8e2e00e5b9a20ede1914669ebd326c3a655fd'
